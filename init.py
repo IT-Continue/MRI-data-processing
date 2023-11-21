8614a0622af5fcbcea7abebbdb8fd62e5e086b0d
Носов Артём Иванович <nai008@campus.mephi.ru>
2023-11-21 15:42:54 +0300

    Update init.py

--- init.py
+++ init.py
@@ -1,8 +1,8 @@
 import os
 from settings import path_to_raw_data, path_to_output
 
-if not os.path.exists(path_to_reports):
-    os.mkdir(path_to_reports)
+if not os.path.exists(path_to_raw_data):
+    os.mkdir(path_to_raw_data)
 
-if not os.path.exists(path_to_recordings):
-    os.mkdir(path_to_recordings)
+if not os.path.exists(path_to_output):
+    os.mkdir(path_to_output)
